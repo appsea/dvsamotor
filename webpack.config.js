2e708a27ddddafc3d94c48da8a070c9153e2a787
appsea <girase.rakesh@gmail.com>
2019-07-22 14:54:17 +0100

    Privacy Policy and Upgrade to 6.0

--- webpack.config.js
+++ webpack.config.js
@@ -229,6 +229,8 @@ module.exports = env => {
             new webpack.DefinePlugin({
                 "global.TNS_WEBPACK": "true",
                 "process": "global.process",
+                "myGlobal.monica": JSON.stringify(env.monica),
+                "myGlobal.gupit": JSON.stringify(env.gupit)
             }),
             // Remove all files from the out dir.
             new CleanWebpackPlugin(itemsToClean, { verbose: !!verbose }),
