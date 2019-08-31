87b4c4e7d954311f3b013eb36c52bdb58da0d8b0
appsea <girase.rakesh@gmail.com>
2019-08-31 23:02:54 +0100

    Disable Menu Button on Blank Page
    Signed-off-by: appsea <girase.rakesh@gmail.com>

--- app/question/blank-page.ts
+++ app/question/blank-page.ts
@@ -1,12 +1,7 @@
 import { EventData } from "tns-core-modules/data/observable";
 import { AdService } from "~/admob/ad.service";
-import { QuestionViewModel } from "~/question/question-view-model";
 import { QuestionService } from "~/services/question.service";
 import * as navigationModule from "../shared/navigation";
-
-export function onDrawerButtonTap(args: EventData) {
-    QuestionViewModel.showDrawer();
-}
 
 export function onPageLoaded(args) {
     AdService.getInstance().delayedPreloadInterstitial();
